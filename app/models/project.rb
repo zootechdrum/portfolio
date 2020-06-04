483a9c5b3f552d966c651e755bc905495223cb49
Cesar Gomez <zootechdrum@gmail.com>
2020-06-04 16:33:24 -0700

    implemented image defatults in model diretory

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,14 @@ class Project < ApplicationRecord
         where(subtitle: 'React')
     end
 
+    after_initialize :set_defaults
+
+    
+    def set_defaults
+        self.main_image  ||=  "http://placehold.it/600x400"
+        self.thumb_image ||= "http://placehold.it/350x200"
+    end
+
 end
+
+
